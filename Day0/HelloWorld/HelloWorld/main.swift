68667f1f2e4f409a34fe9981bcdc0d1748149a44
keepcalmandcodecodecode <igor.github@gmail.com>
2019-01-06 10:56:40 +0400

    finish HelloWorld

---
+++ Day0/HelloWorld/HelloWorld/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  HelloWorld
+//
+//  Created by developer on 1/6/19.
+//  Copyright © 2019 developer. All rights reserved.
+//
+
+import Foundation
+
+let inputString = readLine()!
+print("Hello, World.")
+print(inputString)
