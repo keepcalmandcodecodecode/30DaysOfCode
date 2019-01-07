39d939061635e342834d85cdc1ead07a357a8de5
keepcalmandcodecodecode <igor.github@gmail.com>
2019-01-07 10:39:05 +0400

    complete DataTypes

---
+++ Day0/DataTypes/DataTypes/main.swift
@@ -0,0 +1,32 @@
+//
+//  main.swift
+//  DataTypes
+//
+//  Created by developer on 1/7/19.
+//  Copyright © 2019 developer. All rights reserved.
+//
+
+import Foundation
+
+var i = 4
+var d = 4.0
+var s = "HackerRank "
+
+// Declare second integer, double, and String variables.
+var secondInt: Int
+var secondDouble: Double
+var secondString: String
+
+// Read and save an integer, double, and String to your variables.
+secondInt = Int(readLine()!)!
+secondDouble = Double(readLine()!)!
+secondString = readLine()!
+
+// Print the sum of both integer variables on a new line.
+print(i+secondInt)
+// Print the sum of the double variables on a new line.
+print(d+secondDouble)
+// Concatenate and print the String variables on a new line
+print(s+secondString)
+// The 's' variable above should be printed first.
+
